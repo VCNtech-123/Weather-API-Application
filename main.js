7d51404bd8e40c489839ad78bd558a01b188c874
John Francis Vecina <johnvecina640@gmail.com>
2026-03-23 15:06:39 +0800

    -- added files created the html structure and css styling

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const searchBar = document.querySelector('.search_bar');
+const searchButton = document.querySelector('.search_button');
+
+searchButton.addEventListener('click', () => {
+    
+});
