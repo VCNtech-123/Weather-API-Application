c5883c00a7f940b7511a5a49d34b18099699b962
John Francis Vecina <johnvecina640@gmail.com>
2026-03-23 18:06:10 +0800

    -- minor changes

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const searchButton = document.querySelector('.search_button');
 
 
 searchButton.addEventListener('click', () => {
-    const search = searchBar.value;
+    const search = searchBar.value.trim();
     renderCreate(search);
     searchBar.value = '';
 });
 
 searchBar.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-        const search = e.target.value;
+        const search = e.target.value.trim();
     
         if (search) {
             renderCreate(search);
